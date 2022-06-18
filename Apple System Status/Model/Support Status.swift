e59d7a46d1148628b0dd64f46e9661ca53fe7eb2
Nikita Rossik <born2lose@protonmail.com>
2022-06-18 20:02:13 +0300

    clean up model

--- Apple System Status/Model/Support Status.swift
+++ Apple System Status/Model/Support Status.swift
@@ -11,23 +11,23 @@ import Foundation
 // https://www.apple.com/support/systemstatus/data/system_status_en_US.js
 
 struct SupportStatus {
-	let services: [Services]
+	var services: [Services]
 }
 
 struct Services {
 	var serviceName: String
 	var redirectUrl: String?
-	var events: [Event]?
+	var events: [Event]
 }
 
 struct Event {
-	let usersAffected: String
-	let epochStartDate: Int
-	let epochEndDate: Int?
-	let messageID, statusType, datePosted, startDate: String
-	let endDate: String?
-	let affectedServices: [String]?
-	let eventStatus, message: String
+	var usersAffected: String
+	var epochStartDate: Int
+	var epochEndDate: Int?
+	var messageID, statusType, datePosted, startDate: String
+	var endDate: String?
+	var affectedServices: [String]?
+	var eventStatus, message: String
 
 	enum CodingKeys: String, CodingKey {
 		case usersAffected, epochStartDate, epochEndDate
@@ -35,43 +35,6 @@ struct Event {
 		case statusType, datePosted, startDate, endDate, affectedServices, eventStatus, message
 	}
 }
-
-// extension Services {
-//				static var previewDataJSON: [Services] {
-//								let previewDataURL = Bundle.main.url(forResource: "system_status_ru", withExtension: "json")!
-//								let jsonDecoder = JSONDecoder()
-//
-//								let data = try! Data(contentsOf: previewDataURL)
-//								let apiResponse = try! jsonDecoder.decode(SupportStatus.self, from: data)
-//
-//								return apiResponse.services
-//				}
-// }
-
-// extension Event {
-//				static var localData: [Event] {
-//								[Event(
-//												usersAffected: "Затронуты некоторые пользователи",
-//												epochStartDate: 1647880020000,
-//												epochEndDate: nil,
-//												messageID: "2000001303",
-//												statusType: "Outage",
-//												datePosted: "21.03.2022 17:58 GMT",
-//												startDate: "21.03.2022 16:27 GMT",
-//												endDate: nil,
-//												affectedServices: [
-//																"iCloud Drive",
-//																"Веб-приложения iCloud (iCloud.com)",
-//																"Календарь iCloud",
-//																"Почта iCloud",
-//																"Связка ключей iCloud",
-//																"Учетная запись и вход в iCloud"
-//												],
-//												eventStatus: "ongoing",
-//												message: "Возможно, нет доступа к календарям из веб-приложения «Календарь» на icloud.com."
-//								)]
-//				}
-// }
 
 extension SupportStatus: Decodable {}
 extension Services: Decodable {}
