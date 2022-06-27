4b2c492572de2e3d9aa1b30279d02128ec22d363
Nikita Rossik <born2lose@protonmail.com>
2022-06-27 19:14:53 +0300

    save selected tab to user defaults

--- Apple System Status/UI/NavigationTabView.swift
+++ Apple System Status/UI/NavigationTabView.swift
@@ -12,25 +12,31 @@ struct NavigationTabView: View {
 	@ObservedObject
 	var model: ServiceListModel
 
+	@AppStorage("selectedTab")
+	private var selectedTab: Int = 0
+
 	var body: some View {
-		TabView {
+		TabView(selection: $selectedTab) {
 			// MARK: - Support status
 			ServiceListView(model: model)
 				.tabItem {
 					Label("Support", systemImage: "gear.badge.checkmark")
 				}
+				.tag(0)
 
 			// MARK: - Developer status
 			DeveloperListView(model: model)
 				.tabItem {
 					Label("Developer", systemImage: "hammer.circle")
 				}
+				.tag(1)
 
 			// MARK: - Setting
 			Text("Settings")
 				.tabItem {
 					Label("Settings", systemImage: "gear.circle")
 				}
+				.tag(2)
 		}
 	}
 }
