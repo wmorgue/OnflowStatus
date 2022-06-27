8f510b227c53418674de29c9fd79eb1360d1e337
Nikita Rossik <born2lose@protonmail.com>
2022-06-27 17:59:49 +0300

    clean up view model

--- Apple System Status/Model/ServiceListModel.swift
+++ Apple System Status/Model/ServiceListModel.swift
@@ -11,6 +11,8 @@ import OSLog
 
 @MainActor
 final class ServiceListModel: ObservableObject {
+	let networking = StatusResource()
+
 	@Published
 	var services: [Services] = []
 
@@ -20,7 +22,7 @@ final class ServiceListModel: ObservableObject {
 	@Published
 	var showingSheet: Bool = false
 
-	private let client = APIClient(baseURL: URL(string: "https://www.apple.com/support/systemstatus/data"))
+	//	private let client = APIClient(baseURL: URL(string: "https://www.apple.com/support/systemstatus/data"))
 }
 
 private extension Logger {
@@ -33,12 +35,6 @@ private extension Logger {
 extension ServiceListModel {
 	static let preview = ServiceListModel()
 
-	func debugDump<T>(_ value: T) -> Void {
-		#if targetEnvironment(simulator)
-			dump(value, indent: 2)
-		#endif
-	}
-
 	func showSheet(for service: Services) {
 		guard service.events.isEmpty else {
 			showingSheet.toggle()
@@ -46,52 +42,38 @@ extension ServiceListModel {
 		}
 	}
 
-	func getSupportServices(for language: String = "en_US") async {
-		let statusCode = try? await client
-			.send(.get("/system_status_\(language).js"))
-			.statusCode
-
-		guard statusCode == 200 else {
-			// showing view with retry button insdead a alert with try again button
-			Logger.serviceModel.info("The server responded with an error. Status code: \(statusCode.debugDescription)")
-			return
-		}
-
+	func fetchSupport() async {
 		do {
-			let status: SupportStatus = try await client.send(.get("/system_status_\(language).js")).value
-			//						debugDump(status)
-			services = status.services
+			services = try await networking.fetchSupportServices()
+		} catch {
+			Logger.serviceModel.error("\(error.localizedDescription)")
+		}
+	}
+
+	func fetchDeveloper() async {
+		do {
+			developers = try await networking.fetchDeveloperServices()
 		} catch {
 			Logger.serviceModel.error("\(error)")
 		}
 	}
 
-	func getDeveloperServices(for language: String = "en_US") async {
-		do {
-			// Return a JSON-P with callback so we need to remove a garbage
-			var callbackResult: String = try await client.send(.get("/developer/system_status_\(language).js")).value
-			callbackResult.removeFirst(13)
-			callbackResult.removeLast(3)
+//	enum LocaleTimeZone: String {
+//		case PDT
+//		case GMT
+//	}
 
-			let callbackResultData = callbackResult.data(using: .utf8)!
-
-			let isValidObject: Bool = JSONSerialization.isValidJSONObject(callbackResult)
-
-			guard !isValidObject else {
-				Logger.serviceModel.error("Invalid JSON: \(#function)")
-				return
-			}
-
-			let status: SupportStatus = try! JSONDecoder().decode(SupportStatus.self, from: callbackResultData)
-			//						debugDump(status)
-			developers = status.services
-		} catch {
-			Logger.serviceModel.error("\(error)")
-		}
-	}
+//	func matchNetworkTimezone(_ locale: String) -> String {
+//		switch locale.elementsEqual("en_US") {
+//			case true: return "PDT"
+//			case false: return "GMT"
+//		}
+//	}
 
 	func relativeStartDate(from startDate: String) -> String {
-		let strategy = Date.ParseStrategy(format: "\(month: .twoDigits)/\(day: .twoDigits)/\(year: .defaultDigits) \(hour: .twoDigits(clock: .twelveHour, hourCycle: .zeroBased)):\(minute: .twoDigits) PDT", locale: Locale(identifier: "en_US"), timeZone: .current)
+//		let timezoneLocale: String = matchNetworkTimezone(networking.locale)
+
+		let strategy = Date.ParseStrategy(format: "\(month: .twoDigits)/\(day: .twoDigits)/\(year: .defaultDigits) \(hour: .twoDigits(clock: .twelveHour, hourCycle: .zeroBased)):\(minute: .twoDigits) PDT", locale: Locale(identifier: networking.locale), timeZone: .current)
 		guard let date = try? Date(startDate, strategy: strategy) else { return Date.now.formatted() }
 
 		let formatter = RelativeDateTimeFormatter()
