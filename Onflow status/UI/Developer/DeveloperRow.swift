7f33eb5e949da9fc5f56170d3ea819df425ad2cf
Nikita Rossik <born2lose@protonmail.com>
2022-07-06 19:40:57 +0300

    .labelStyle doesn't support ternary operator

--- Onflow status/UI/Developer/DeveloperRow.swift
+++ Onflow status/UI/Developer/DeveloperRow.swift
@@ -7,15 +7,11 @@
 
 import SwiftUI
 
-fileprivate struct DeveloperTextRow: View {
+struct DeveloperRow: View {
 
 	var service: Services
 
 	@Environment(\.openURL) private var openURL
-
-	init(_ service: Services) {
-		self.service = service
-	}
 
 	fileprivate func openUnwrapURL() {
 		if let link = service.redirectUrl {
@@ -27,47 +23,99 @@ fileprivate struct DeveloperTextRow: View {
 	}
 
 	var body: some View {
-		Label {
-			switch service.redirectUrl == nil {
-			case true:
-				Text(service.serviceName)
-			case false:
-				Label(service.serviceName, systemImage: "link")
-					.labelStyle(.reversed)
-			}
-		} icon: {
-			Image(systemName: "circle.dotted")
-				.foregroundColor(setCircleColor)
-		}
-		.foregroundColor(service.events.isEmpty ? .primary : .blue)
-		.lineLimit(1)
-		.minimumScaleFactor(0.8)
-		.onTapGesture(count: 2) {
+		Button {
 			openUnwrapURL()
+		} label: {
+			HStack {
+				Image(systemName: "circle.dotted")
+					.foregroundColor(setCircleColor)
+
+				// ternary operator doesn't work
+				// https://useyourloaf.com/blog/adapting-swiftui-label-style/
+				if service.redirectUrl != nil {
+					Label(service.serviceName, systemImage: "link")
+						.foregroundColor(.blue)
+						.lineLimit(1)
+						.minimumScaleFactor(0.9)
+						.labelStyle(.reversed)
+				} else {
+					Label(service.serviceName, systemImage: "link")
+						.foregroundColor(.primary)
+						.lineLimit(1)
+						.minimumScaleFactor(0.9)
+						.labelStyle(.titleOnly)
+				}
+			}
 		}
 	}
 }
 
-extension DeveloperTextRow {
+extension DeveloperRow {
 	var setCircleColor: Color {
 		service
 			.events
 			.map(\.eventStatus)
 			.contains("completed") || service.events.isEmpty ? .green : .orange
 	}
+
+//	var dynamicLable: some View {
+//		Label(service.serviceName, systemImage: "link")
+//			.foregroundColor(.primary)
+//			.lineLimit(1)
+//			.minimumScaleFactor(0.9)
+//			.labelStyle(service.redirectUrl == .none ? .titleOnly : .iconOnly)
+//	}
 }
 
-struct DeveloperRow: View {
-
-	var service: Services
-
-	var body: some View {
-		DeveloperTextRow(service)
-	}
-}
+// fileprivate struct DeveloperTextRow: View {
+//
+//	var service: Services
+//
+//	@Environment(\.openURL) private var openURL
+//
+//	init(_ service: Services) {
+//		self.service = service
+//	}
+//
+//	fileprivate func openUnwrapURL() {
+//		if let link = service.redirectUrl {
+//			// sometimes links have a whitespace and you can't open a link.
+//			if let url = URL(string: link.trim) {
+//				openURL(url)
+//			}
+//		}
+//	}
+//
+//	var body: some View {
+//		Label {
+//			switch service.redirectUrl == nil {
+//			case true:
+//				Text(service.serviceName)
+//			case false:
+//				Label(service.serviceName, systemImage: "link")
+//					.labelStyle(.reversed)
+//			}
+//		} icon: {
+//			Image(systemName: "circle.dotted")
+//				.foregroundColor(setCircleColor)
+//		}
+//		.lineLimit(1)
+//		.minimumScaleFactor(0.8)
+//		.onTapGesture { openUnwrapURL() }
+//	}
+// }
+//
+// extension DeveloperTextRow {
+//	var setCircleColor: Color {
+//		service
+//			.events
+//			.map(\.eventStatus)
+//			.contains("completed") || service.events.isEmpty ? .green : .orange
+//	}
+// }
 
 struct DeveloperRow_Previews: PreviewProvider {
 	static var previews: some View {
-		DeveloperTextRow(MockData.developerService)
+		DeveloperRow(service: MockData.developerService)
 	}
 }
