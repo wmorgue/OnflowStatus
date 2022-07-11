b66d58de2e2a1b36b459169e7706b9400bff5d83
Nikita Rossik <born2lose@protonmail.com>
2022-07-11 11:26:14 +0300

    New UI Components

---
+++ Onflow status/UI/Components/SortListButton.swift
@@ -0,0 +1,32 @@
+//
+//  SortListButton.swift
+//  Onflow status
+//
+//  Created by Nikita Rossik on 7/10/22.
+//
+
+import SwiftUI
+
+struct SortListButton: View {
+
+	@Binding
+	var toggleButton: Bool
+	let isDisabledReason: () -> Bool
+
+	var body: some View {
+		Button {
+			toggleButton.toggle()
+		} label: {
+			Image(systemName: "arrow.up.arrow.down.square")
+				.foregroundStyle(Color.sortEventsButton)
+				.symbolRenderingMode(.hierarchical)
+		}
+		.disabled(isDisabledReason())
+	}
+}
+
+struct SortListButton_Previews: PreviewProvider {
+	static var previews: some View {
+		SortListButton(toggleButton: .constant(false)) { false }
+	}
+}
