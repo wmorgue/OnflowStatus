6c699298cc95d21ef91c0cd80b8a3efe44e1132c
Nikita Rossik <born2lose@protonmail.com>
2022-07-11 11:24:08 +0300

    Remove Ext extensions

---
+++ Onflow status/Extensions/Color.swift
@@ -0,0 +1,12 @@
+//
+//  ColorExtension.swift
+//  Onflow status
+//
+//  Created by Nikita Rossik on 7/10/22.
+//
+
+import SwiftUI
+
+extension Color {
+	static let sortEventsButton = Color("FilterEventsButton")
+}
