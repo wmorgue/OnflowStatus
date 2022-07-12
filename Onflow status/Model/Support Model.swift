a7c60c1d72a4642756a3360d8372afdb370866d1
Nikita Rossik <born2lose@protonmail.com>
2022-07-12 04:44:45 +0300

    bump pbxproj

--- Onflow status/Model/Support Model.swift
+++ Onflow status/Model/Support Model.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-// Support Status
-// https://www.apple.com/support/systemstatus/data/system_status_en_US.js
-
-// Developer Status
-// https://www.apple.com/support/systemstatus/data/developer/system_status_en_US.js
+/*
+ Support Status
+ https://www.apple.com/support/systemstatus/data/system_status_en_US.js
+ Developer Status
+ https://www.apple.com/support/systemstatus/data/developer/system_status_en_US.js
+ */
 
 struct SupportStatus {
 	var services: [Services]
