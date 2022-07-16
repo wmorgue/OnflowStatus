eb9b7be9c3d556809965d6acaac7014315099ecd
Nikita Rossik <born2lose@protonmail.com>
2022-07-17 00:43:53 +0300

    Feature: Backport for new iOS 15/16

---
+++ Onflow status/Backport/NavigationBackport.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationBackport.swift
+//  Onflow status
+//
+//  Created by Nikita Rossik on 7/16/22.
+//
+
+import SwiftUI
+
+public struct NavigationBackport<Content: View>: View {
+	public let content: () -> Content
+
+	public var body: some View {
+		if #available(iOS 16, *) {
+			return NavigationStack { content() }
+		} else {
+			return NavigationView { content() }
+		}
+	}
+}
+
+
+struct NavigationBackport_Previews: PreviewProvider {
+	static var previews: some View {
+		NavigationBackport {
+				Text("Is working")
+				.navigationBarTitle("Backport")
+		}
+	}
+}
