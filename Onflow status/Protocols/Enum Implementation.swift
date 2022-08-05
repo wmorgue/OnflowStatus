ed8fee17c7d6e8952b8b87f1696d04b9466e1f0d
Nikita Rossik <born2lose@protonmail.com>
2022-08-05 14:52:09 +0300

    Move `Impl` to Protocols dir

---
+++ Onflow status/Protocols/Enum Implementation.swift
@@ -0,0 +1,32 @@
+//
+//  Enum Implementation.swift
+//  Onflow status
+//
+//  Created by Nikita Rossik on 6/17/22.
+//
+
+import Foundation
+
+enum EventStatusMessage {
+	case support
+	case developer
+
+	var text: String {
+		switch self {
+		case .support: return "resolved"
+		case .developer: return "resolved" // before: completed
+		}
+	}
+}
+
+enum AppIcon {
+	case dark
+	case light
+
+	var iconName: String? {
+		switch self {
+		case .dark: return nil
+		case .light: return "LightIcon"
+		}
+	}
+}
