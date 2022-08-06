500e7eb2c8c755a998bba362a0a6e3e5f891231f
Nikita Rossik <born2lose@protonmail.com>
2022-08-06 16:48:26 +0300

    Brand new `MockDataServices`

---
+++ Onflow status/ViewModel/MockDataServices.swift
@@ -0,0 +1,34 @@
+//
+//  MockDataServices.swift
+//  Onflow status
+//
+//  Created by Nikita Rossik on 6/16/22.
+//
+//
+import Foundation
+import Get
+import OnflowNetwork
+
+struct MockStatusService: OnflowServiceProtocol {
+	var host: APIClient = .init(baseURL: URL(string: "https://www.apple.com/support/systemstatus"))
+
+	var requestPath: OnflowNetwork.RequestPath = .support
+
+	var localeLayer: OnflowNetwork.LocaleLayer = .instance
+
+	/// Реквест на тестовый сервер
+	func performRequest(_ requestPath: OnflowNetwork.RequestPath) -> Get.Request<OnflowNetwork.SupportStatus> {
+		Request.get(requestPath.path + localeLayer.locale + RequestConstant.requestPathExtension)
+	}
+
+	/// Get data from JSON file
+	func fetchServices() async throws -> [OnflowNetwork.Services] {
+		guard let url = Bundle.main.url(forResource: "system_status_ru", withExtension: "json"),
+		      let data = try? Data(contentsOf: url) else { return [] }
+
+		let jsonDecoder = JSONDecoder()
+		let apiResponse = try? jsonDecoder.decode(SupportStatus.self, from: data)
+
+		return apiResponse?.services ?? []
+	}
+}
