1980dd68e113fde24fccbd0f0dce334dbbc05d37
Nikita Rossik <born2lose@protonmail.com>
2022-09-10 12:42:36 +0300

    merge files

--- Onflow status/Impl + Protocols/ContactsProtocol.swift
+++ Onflow status/Impl + Protocols/ContactsProtocol.swift
@@ -1,11 +1,26 @@
 //
-//  Contact Implementation.swift
+//  ContactsProtocol.swift
 //  Onflow status
 //
-//  Created by Nikita Rossik on 7/1/22.
+//  Created by Nikita Rossik on 7/31/22.
 //
 
 import Foundation
+
+/// Contains contacts links
+protocol ContactsProtocol {
+	/// E-mail to me
+	var mail: URL { get }
+
+	/// Telegram link to me
+	var telegram: URL { get }
+
+	/// Link to App Store (if the link exists)
+	var appStore: URL { get }
+
+	/// Link to TestFlight
+	var testFlight: URL { get }
+}
 
 struct TestFlightContact: ContactsProtocol {
 	let mail = URL(string: "mailto:maybequantumbit@icloud.com")!
